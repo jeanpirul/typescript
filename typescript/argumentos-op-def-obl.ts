25a8003637e3277536f5599f31ff98a44e10bcad
Jean Pirul <jean.pirul@trebol-it.cl>
2021-09-14 13:04:53 -0300

    fix: se sube mejora de codigo de prueba

--- typescript/argumentos-op-def-obl.ts
+++ typescript/argumentos-op-def-obl.ts
@@ -1,5 +1,14 @@
 (function () {
   //argumentos opcionales, por defecto y obligatorios
+  function activar(quien: string, momento?: string, objeto: string = 'batiseñal') {
+    if (momento) {
+      console.log(`${quien} activó la ${objeto} en la ${momento}`)
+    } else {
+      console.log(`${quien} activó la ${objeto}`)
 
+    }
+  }
+
+  activar('Gordon', 'tarde');
 })();
 
