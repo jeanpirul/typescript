9d73fe9c5566733f9a557a331ce416b8b20b026f
Jean Pirul <jean.pirul@trebol-it.cl>
2021-09-15 15:58:14 -0300

    se revisan las clases y sus partes

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
 "use strict";
 (() => {
-    //empezar a ver desde el video 17
-    class Avenger {
-    }
-    const antman = new Avenger();
-    console.log(antman);
 })();
--- app.ts
+++ app.ts
@@ -1,17 +1,4 @@
 (() => {
-    //empezar a ver desde el video 17
-    class Avenger {
-
-        nombre: string;
-        equipo: string;
-        nombreReal: string;
-
-        puedePelear: boolean;
-        peleasGanadas: number;
-    }
-
-    const antman = new Avenger();
-    console.log(antman)
 
 })();
 
